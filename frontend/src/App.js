677b39bbc1de7cb4b32cef05838af83aa067ff3a
Zeyu-Li <zeyu.li23@gmail.com>
2021-11-10 21:38:06 -0700

    animations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    setOpen(true);
+  }, []);
+
   return (
     <div className="App">
       <main className="App-header">
-        <div>
-          <h1>Hi</h1>
+        <div
+          className="main-container"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "100vh",
+            backgroundColor: "#fafafa",
+            color: "#333333",
+          }}
+        >
+          <h1
+            className="transition"
+            style={{
+              opacity: open ? 1 : 0,
+            }}
+          >
+            Analyzer
+          </h1>
         </div>
       </main>
     </div>
