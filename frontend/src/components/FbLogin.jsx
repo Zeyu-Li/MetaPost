b36917010b0c7309134745028f061e5aabc48b4e
Vardan Saini <Vardan1@ualberta.ca>
2021-11-19 20:07:27 -0700

    Login page UI updates

--- frontend/src/components/FbLogin.jsx
+++ frontend/src/components/FbLogin.jsx
@@ -1,14 +1,38 @@
 import FacebookLogin from 'react-facebook-login'
 
+const divStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column'
+};
+
 const FbLogin = (props) => {
     return (
       <>
+      
+      <div style={divStyle}>
+      <h1 style={{fontsize:30}} > 
+          MetaPost  
+        </h1>
+      <img
+          src={"/logo.png"}
+          alt={"logo"}
+          style={{ height: 500, width: 500}}
+        />
           <FacebookLogin
               appId="432233278303617"
               autoLoad={true}
               fields="name,email,picture"
               // onClick={componentClicked}
               callback={props.fbResponse} />
+        <h1
+          
+          style={{
+            opacity: 1,
+          }}
+        ></h1>
+      </div>
+          
 
       </>
     );
