9f2e54767d532a2052e64eb949d77f1b164a40ee
Zeyu-Li <zeyu.li23@gmail.com>
2021-11-19 20:49:29 -0700

    fixed build

--- frontend/src/components/FbLogin.jsx
+++ frontend/src/components/FbLogin.jsx
@@ -1,37 +1,21 @@
 import FacebookLogin from 'react-facebook-login'
 
-const divStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  flexDirection: 'column'
-};
-
-const logoStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  flexDirection: 'row'
-};
-
 const FbLogin = (props) => {
     return (
       <>
-      <div style={divStyle}>
-        <div style={logoStyle}>
+      <div className="div-style flex-column">
+        <div className="div-style flex-row">
           <h1 style={{fontSize:120}} > 
           MetaPost  
         </h1>
       <img
           src={"/logo.png"}
           alt={"MetaPost logo"}
-          alt={"logo"}
           style={{ height: 250, width: 250}}
         />
         </div>
         <p style={{fontSize:32, maxWidth: 800, margin: 0}}>An app made to make social media a more warm and accessible place for everyone.</p>
-        <h1
-          style={{ fontSize: 100,
-            opacity: 1,
-          }}></h1>
+        <div style={{ height: 100 }} ></div>
     
           <FacebookLogin
               appId="432233278303617"
@@ -39,9 +23,7 @@ const FbLogin = (props) => {
               fields="name,email,picture"
               // onClick={componentClicked}
               callback={props.fbResponse} />
-        <div
-          style={{ height: 100 }}
-        ></div>
+        <div style={{ height: 100 }} ></div>
       </div>
           
 
